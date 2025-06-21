0754e770a11bc01add7417611d700e5652d6c471
Sergio Gaiotto <sergio.gaiotto@gmail.com>
2025-06-21 19:47:08 -0300

    Add files via upload

---
+++ agents/__init__.py
@@ -0,0 +1,9 @@
+# Módulo agents - Agentes de IA para pesquisa e análise
+
+__version__ = "1.0.0"
+__author__ = "FIA.LabData - Prof Sergio Gaiotto"
+
+from .mcp_agent import MCPAgent
+from .workflow_agent import WorkflowAgent
+
+__all__ = ["MCPAgent", "WorkflowAgent"]
